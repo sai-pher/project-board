22bb5908d0322b863886f46c24dae66f7713ad5f
sai-pher <arimanwd@gmail.com>
2021-05-27 17:54:47 +0000

    completed test UI and verified end-to-end functionality of UI and server

--- ag_view/projectBoardView/src/app/app.module.ts
+++ ag_view/projectBoardView/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddProjectComponent } from './component/add-project/add-project.component';
+import { ProjectDetailsComponent } from './component/project-details/project-details.component';
+import { ProjectsListComponent } from './component/projects-list/projects-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddProjectComponent,
+    ProjectDetailsComponent,
+    ProjectsListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- njs_service/server.js
+++ njs_service/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
+var cors = require('cors')
 const db = require("./app/models");
 
 const app = express();
 
+const corsOptions = {
+    origin: "http://localhost:4200"
+  };
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
@@ -25,6 +31,8 @@ require("./app/routes/project.routes")(app);
 
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 })
+
+server.setTimeout(50000);
